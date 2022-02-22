b6d20e8dcd5c6deb8a1659f5404d9f11f7137b68
burak432 <burak.atilla@gmail.com>
2022-02-22 05:50:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-azwpr9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-azwpr9)
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Basic from "./App";
+
+ReactDOM.render(<Basic />, document.getElementById("root"));
