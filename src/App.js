fcdf2c7b973912d2ed5e509edb2a393f87dc67a9
burak432 <burak.atilla@gmail.com>
2022-02-23 08:59:07 +0300

    1 | radio buttons

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ const Basic = () => (
         firstName: '',
         lastName: '',
         email: '',
+        gender: '',
       }}
       onSubmit={async (values) => {
         await new Promise((r) => setTimeout(r, 500));
         alert(JSON.stringify(values, null, 2));
+        console.log(values);
       }}
     >
-      {({ handleSubmit, handleChange }) => (
+      {({ handleSubmit, handleChange, values }) => (
         <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="firstName">First Name</label>
@@ -24,15 +26,46 @@ const Basic = () => (
           <br />
           <div>
             <label htmlFor="lastName">Last Name</label>
-            <input id="lastName" name="lastName" />
+            <input id="lastName" name="lastName" onChange={handleChange} />
           </div>
           <br />
           <div>
             <label htmlFor="email">Email</label>
-            <input id="email" name="email" type="email" />
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={handleChange}
+            />
           </div>
           <br />
+          <br />
+          <label htmlFor="male">male</label>
+          <input
+            type="radio"
+            id="male"
+            name="gender"
+            value="male"
+            onChange={handleChange}
+          />
+          <br />
+          <label htmlFor="female">female</label>
+          <input
+            type="radio"
+            id="female"
+            name="gender"
+            value="female"
+            onChange={handleChange}
+          />
+
+          <br />
+          <br />
+
           <button type="submit">Submit</button>
+
+          <br />
+          <br />
+          <div>{JSON.stringify(values)}</div>
         </form>
       )}
     </Formik>
